13e3d274ae52bbe109675fd45c9ca6f9405c0ded
Thomas Haddy <thomasclh1997@gmail.com>
2021-03-15 20:36:20 -0500

    Updating things. Got a structure figured out

---
+++ start-mirror.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+   # Call build-mirror.py
+
+   # Call install-mirror.py
+
+   # Call run-mirror.py
+
+   print("Done")
+
+if __name__ == "__main__":
+   main()
